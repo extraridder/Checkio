25566299f5c9a0d5cf7e5a573eb814590390788c
pshalagin <extraridder@gmail.com>
2016-11-08 15:22:18 +0300

    Condition depends on step of iteration

--- Bird_Language.py
+++ Bird_Language.py
@@ -6,51 +6,54 @@ def translate(phrase):
     out = ""
     for each in words:
         print("Start each word",  len(each), each, out)
-
-        for i in range(0, len(each)):
-            if i == 0:
-                if each[i] not in VOWELS and each[i + 1] in VOWELS:
+        if len(each) == 2:
+            # print("len = 2")
+            for i in range(0,len(each)):
+                if each[i] not in VOWELS and each[i+1] in VOWELS:
+                    # print("condition active")
                     out += each[i]
-                elif each[i] in VOWELS and each[i-1] not in VOWELS:
-                    continue
-            elif i > 0 and i < len(each) - 2:
-                if each[i] in VOWELS and each[i-1] not in VOWELS:
-                    continue
-                elif 1:
-                    pass
+                    break
+        elif len(each) == 3:
+            #print("len 3")
+            for i in range(0, len(each)):
+                if (i == 0 or i == 1) and (each[i] not in VOWELS and each[i + 1] in VOWELS):
+                    if each[i] not in VOWELS and each[i + 1] in VOWELS:
+                        # print("condition active")
+                        out += each[i]
+                        break
+                elif i == 0 and (each[i] in VOWELS and each[i] == each[i+1] == each[i+2]):
+                    # print("trigger VOWEL len =3")
+                    out += each[i]
+        else:
+            for i in range(0, len(each)):
+                if i == 0:
+                    if each[i] not in VOWELS and each[i+1] in VOWELS:
+                        #print("condition active")
+                        out += each[i]
+                    elif each[i] in VOWELS and each[i] == each[i+1] == each[i+2]:
+                        #print("condition active")
+                        out += each[i]
+                elif (i > 0) and i < len(each) - 3:
+                    if each[i] not in VOWELS and each[i+1] in VOWELS:
+                        #print("condition active")
+                        out += each[i]
+                    elif each[i] in VOWELS and each[i] == each[i+1] == each[i+2] and each[i-1] not in VOWELS:
+                        print("condition active")
+                        out += each[i]
+                elif i >= len(each) - 3:
+                    if each[i] not in VOWELS and each[i+1] in VOWELS:
+                        #print("condition active")
+                        out += each[i]
 
+        out += " "
 
-
-
-            '''
-            print("Start next elem",i, each[i], len(each), each,  out )
-            if each[i] not in VOWELS and each[i + 1] in VOWELS:
-                print("not Vowel st",i, each[i], len(each), each, out)
-                out += each[i]
-                print("not Vowel end",i, each[i], len(each), each, out)
-            elif i == (len(each) - 2):
-                out += " "
-            elif each[i] in VOWELS and (each[i]== each[i+1]==each[i+2]):
-                print("Vowel st",i, each[i], len(each), each, out)
-                out += each[i]
-                print("Vowel end",i, each[i], len(each), each, out)
-            else:
-                print("else")
-                continue
-            '''
-            '''
-            if i == (len(each) - 1):
-                print("last element st",i, each[i], len(each), each, out)
-                out += " "
-                print("last element end",i, each[i], len(each), each, out)
-            '''
     print(out)
 
 
 
     return phrase
 
-
+#translate("yyyooouuu")
 translate("hoooowe yyyooouuu duoooiiine")
 '''
 if __name__ == '__main__':
