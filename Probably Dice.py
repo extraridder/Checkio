5ad262c4688652191ab4029c9a66905e6e0c0fa8
Elle <Elle>
2016-12-04 13:27:33 +0300

    Successful solving "Probably Dice" task

--- Probably Dice.py
+++ Probably Dice.py
@@ -1,15 +1,30 @@
 def probability(dice_number, sides, target):
-    return 0.0
+    dict_probability = {}
+    c = 0
+    for i in range(1, dice_number + 1):
+        if i == 1:
+            for each in range(1, sides + 1):
+                dict_probability[each] = round((1/sides), 4)
+        else:
+            temp_dict = {}
+            for each in dict_probability.keys():
+                for ii in range(1, sides + 1):
+                    if (each + ii) in temp_dict.keys():
+                        temp_dict[each + ii] += (dict_probability[each] * (1 / sides))
+                    elif (each + ii) not in temp_dict.keys():
+                        temp_dict[each + ii] = dict_probability[each] * (1 / sides)
+            dict_probability = temp_dict
+    if target in dict_probability:
+        return dict_probability[target]
+    else:
+        return round(0.0000, 4)
 
-'''
-assert (almost_equal(probability(2, 6, 3), 0.0556)), "Basic example"
-assert (almost_equal(probability(2, 6, 4), 0.0833)), "More points"
-assert (almost_equal(probability(2, 6, 7), 0.1667)), "Maximum for two 6-sided dice"
-assert (almost_equal(probability(2, 3, 5), 0.2222)), "Small dice"
-assert (almost_equal(probability(2, 3, 7), 0.0000)), "Never!"
-assert (almost_equal(probability(3, 6, 7), 0.0694)), "Three dice"
-assert (almost_equal(probability(10, 10, 50), 0.0375)), "Many dice, many sides"
-'''
 
-
-
+probability(2, 6, 3)
+probability(2, 6, 4)
+probability(2, 6, 7)
+probability(2, 3, 5)
+probability(2, 3, 7)
+probability(7, 6, 7)
+probability(10, 10, 50)
+probability(1,2,999)
