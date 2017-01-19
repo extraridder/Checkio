82e7efaa3dcca83bfce76d389bb830febfd35d4f
pshalagin <extraridder@gmail.com>
2017-01-19 17:54:07 +0300

    Wrong family is resolved

--- Wrong family.py
+++ Wrong family.py
@@ -1,22 +1,13 @@
-
 def is_family(tree):
-    #print(tree)
     a = enumerate(tree)
     for i,s in a:
-        #print("START LOOP",i,s)
         if len(tree) == 1:
-            #print("True len==1")
-            print("True")
             return True
     for i in range(1,len(tree)):
-        print(i, tree[i],tree[i][0],[tree[x][1] for x in range(0,i)])
-        if tree[i][0] in [tree[x][1] for x in range(0,i)] or tree[i][0] in [tree[x][0] for x in range(0,i)] and tree[i][1] not in [tree[x][0] for x in range(0,i)]:
-            print("True")
+        if (tree[i][0] in [tree[x][1] for x in range(0,i)] or tree[i][0] in [tree[x][0] for x in range(0,i)]) and (tree[i][1] not in [tree[x][0] for x in range(0,i)] and tree[i][1] not in [tree[x][1] for x in range(0,i)]):
             continue
         else:
-            print("False")
             return False
-    print("True")
     return True
 
 '''
@@ -34,4 +25,5 @@ if __name__ == "__main__":
 #is_family([['Logan', 'Mike']])
 #is_family([['Logan', 'Mike'],['Logan', 'Jack']])
 #is_family([['Logan', 'William'],['Logan', 'Jack'],['Mike', 'Alexander']])
-is_family([["Logan","Mike"],["Logan","Jack"],["Mike","Logan"]])
+#is_family([["Logan","Mike"],["Logan","Jack"],["Mike","Logan"]])
+is_family([["Logan","Mike"],["Logan","Jack"],["Mike","Jack"]])
